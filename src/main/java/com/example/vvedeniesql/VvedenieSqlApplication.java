69e9687e9fe6f73a4d938f9a3d92ba2986a73e02
Alexasc <923248@mail.ru>
2023-05-12 02:16:51 +0700

    Initial commit

---
+++ src/main/java/com/example/vvedeniesql/VvedenieSqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.vvedeniesql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VvedenieSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VvedenieSqlApplication.class, args);
+    }
+
+}
